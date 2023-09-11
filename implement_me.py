61586ddc5d2e138cf79fe03d1eba172e79eb36fa
Danial Riaz <danialriaz91@gmail.com>
2023-09-11 09:23:42 +0200

    updated LCM

--- implement_me.py
+++ implement_me.py
@@ -1,5 +1,14 @@
 
-def lcm(a, b):
-  """compute Least Common Multiple between two integers."""
+import math
 
-  pass
+def lcm(a, b):
+    """Compute the Least Common Multiple between two integers."""
+    # Calculate the LCM using the formula LCM(a, b) = (a * b) / GCD(a, b)
+    return abs(a * b) // math.gcd(a, b)
+
+# Example usage:
+num1 = 12
+num2 = 18
+
+result = lcm(num1, num2)
+print(f"The LCM of {num1} and {num2} is {result}")
